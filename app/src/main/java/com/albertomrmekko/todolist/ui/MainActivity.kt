c092d28155d91fa845ad1be9af14e5f1004dd043
AlbertoMrMekko <albertomartinsanz1@gmail.com>
2026-01-03 22:20:28 +0100

    remove unused imports

--- app/src/main/java/com/albertomrmekko/todolist/ui/MainActivity.kt
+++ app/src/main/java/com/albertomrmekko/todolist/ui/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
 import androidx.navigation.compose.rememberNavController
-import com.albertomrmekko.todolist.ui.group.GroupScreen
 import com.albertomrmekko.todolist.ui.navigation.NavGraph
 import com.albertomrmekko.todolist.ui.theme.ToDoListTheme
 import dagger.hilt.android.AndroidEntryPoint
